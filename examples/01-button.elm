e759966318e494a4e442a2bd2b4f83617fdb8c86
Neil de Carteret <ndecarteret@sportingindex.com>
2019-03-01 14:32:57 +0000

    mucking about

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -1,7 +1,7 @@
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-
+import Html exposing (Html, button, div, text, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (value)
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -9,33 +9,55 @@ main =
 
 -- MODEL
 
-type alias Model = Int
+type alias Model = { total: Int, field: String, error: Maybe String }
 
 init : Model
 init =
-  0
+  { total = 0, field = "1", error = Nothing }
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Apply | SetValue String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      { model | total = (model.total + 1) }
 
     Decrement ->
-      model - 1
+      { model | total = (model.total - 1) }
+
+    Apply ->
+      case String.toInt model.field of
+        Nothing -> { model | error = Just ("Unable to parse \"" ++ model.field ++ "\"")}
+        Just x -> { model | total = model.total + x, error = Nothing }
+
+    SetValue x ->
+      { model | field = x}
 
 
--- VIEW
+-- VIEW 
 
 view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+view {total, field, error} =
+  div [] 
+    [ button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Decrement ] [ text "-" ]
+    , div [ ] [ text (String.fromInt total) ]
+    , div [] [ text (Maybe.withDefault "" error) ]
+    , div [] 
+      [ input 
+        [ value field
+        , onInput SetValue
+        ] 
+        [ ]
+      , button [ onClick Apply ] [ text "Apply" ]
+      ]
+    ] 
+
+isOne : Int -> Bool
+isOne x = case x of
+  1 -> True
+  _ -> False
